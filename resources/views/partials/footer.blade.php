8eab142911b9a81469eff9897b19a2779fd8b26d
Tamar <tamar@voiceworks.nl>
2023-04-13 13:50:33 +0200

    Relative link

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -36,7 +36,7 @@
         </div>
         <div class="row">
             <div class="col-sm-12">
-                <p><a href = "https://status.enreach.tech/rss">Status RSS Feed</a> || <a href="https://status.enreach.tech/rss/json.php">Status JSON</a></p>
+                <p><a href = "./rss">Status RSS Feed</a> || <a href="./rss/json.php">Status JSON</a></p>
             </div>
 
         </div>
