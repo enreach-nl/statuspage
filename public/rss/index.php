b5b6ef678696067649972f9f9f8c7c3f49ed02cc
Tamar <tamar@voiceworks.nl>
2023-05-11 16:15:37 +0200

    Much nice! Multi wow!

--- public/rss/index.php
+++ public/rss/index.php
@@ -1,6 +1,18 @@
 <?php
 error_reporting(0);
 
+function generateV4UUID()
+{
+    return sprintf(
+        '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+        mt_rand(0, 0xffff),
+        mt_rand(0, 0x0fff) | 0x4000,
+        mt_rand(0, 0x3fff) | 0x8000,
+        mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+    );
+}
+
 // get the database connection details
 include ('/etc/statuspage_rss.conf');
 
@@ -41,7 +53,8 @@ foreach ($results as $item) {
 
 // Set the RSS feed title and link
 $rssTitle = "Enreach Service Status";
-$rssLink = "https://" . $_SERVER['HTTP_HOST'] . "/rss";
+$rssLink = "https://" . $_SERVER['HTTP_HOST'] . "/rss/";
+$rssDescription = "A feed for service status updates.";
 
 // Create the XML document
 $xmlDoc = new DOMDocument('1.0', 'utf-8');
@@ -57,10 +70,19 @@ $channel = $xmlDoc->createElement('channel');
 $rss->appendChild($channel);
 
 // Add the title and link to the channel element
-// $title = $xmlDoc->createElement('title', $rssTitle);
-$title = $xmlDoc->createElement('title', htmlspecialchars($service['service'], ENT_QUOTES, 'UTF-8'));
+$title = $xmlDoc->createElement('title', $rssTitle);
+$description = $xmlDoc->createElement('description', $rssDescription);
+
+// Create a DateTime object for the current date and time
+$currentDateTime = new DateTime();
+
+// Create lastBuildDate element with the current date and time in a 4-digit year format
+$lastBuildDate = $xmlDoc->createElement('lastBuildDate', $currentDateTime->format('D, d M Y H:i:s O'));
 
 $channel->appendChild($title);
+$channel->appendChild($description);
+$channel->appendChild($lastBuildDate);
+
 
 $link = $xmlDoc->createElement('link', $rssLink);
 $channel->appendChild($link);
@@ -83,6 +105,12 @@ foreach ($groupedArray as $category => $services) {
         $categoryElement = $xmlDoc->createElement('category', htmlspecialchars($category, ENT_QUOTES, 'UTF-8'));
 
         $item->appendChild($categoryElement);
+
+        // Create a GUID for each item, using the service['id'] as a unique identifier (or any other unique value from your data)
+        $guidValue = generateV4UUID();
+        $guid = $xmlDoc->createElement('guid', htmlspecialchars('https://'.$_SERVER['HTTP_HOST'].'/donotuse/' . $guidValue, ENT_QUOTES, 'UTF-8'));
+        $guid->setAttribute('isPermaLink', 'false');
+        $item->appendChild($guid);
     }
 }
 
